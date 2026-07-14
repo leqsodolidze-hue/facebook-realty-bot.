bf2a5e4c3dc2a5c34b72e7f4e210da8a6d9592ee
leqsodolidze-hue <leqsodolidze@gmail.com>
2026-07-14 17:57:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from flask import Flask, request
 import os
-import requests
 
 app = Flask(__name__)
-VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN') # Render-იდან აიღებს secret123-ს
-PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN') # ესეც დაგვჭირდება მესიჯების გასაგზავნად
+VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN') 
+PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/webhook', methods=['GET']) # << ეს უკვე გაქვს
 def verify():
-    # Facebook აქ ამოწმებს
     token = request.args.get('hub.verify_token')
     challenge = request.args.get('hub.challenge')
     if token == VERIFY_TOKEN:
-        return challenge 
+        return challenge
     return 'Error: Wrong token', 403
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['POST']) # <<<< ეს დაუმატე ბოლოში
 def webhook():
-    # აქ მოგვივა მესიჯები
     data = request.get_json()
-    print(data)
-    return 'ok', 200
+    print("Facebook sent:", data)
+    return "ok", 200
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 10000))
+if __name__ == '__main__':
+    app.run()
